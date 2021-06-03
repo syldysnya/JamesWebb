8f69a6798d0d3c83e29b8a44b6744d61a4ba09dc
Syldys <syldysnya@gmail.com>
2021-06-02 20:33:02 -0400

    Finish hexagons and styled launch countdown

--- src/index.js
+++ src/index.js
@@ -6,28 +6,26 @@ let i = document.getElementById('menu').childNodes;
 
 centerCell.addEventListener('click', function () {
 
-    this.classList.add("transparent")
+    this.classList.toggle("transparent")
 
-    i[1].style.transform = 'translateY(-146px)';
-    i[3].style.transform = 'translate(125px, -73px)';
-    i[5].style.transform = 'translate(125px, 73px)';
-    i[7].style.transform = 'translateY(146px)';
-    i[9].style.transform = 'translate(-125px, 73px)';
-    i[11].style.transform = 'translate(-125px, -73px)';
+    i[1].style.transform = 'translateY(-14.3vh)';
+    i[3].style.transform = 'translate(12.6vh, -102.5%)';
+    i[5].style.transform = 'translate(12.6vh, 102.5%)';
+    i[7].style.transform = 'translateY(14.3vh)';
+    i[9].style.transform = 'translate(-12.6vh, 102.5%)';
+    i[11].style.transform = 'translate(-12.6vh, -102.5%)';
     
-    i[13].style.transform = 'translateY(-292px)';
-    i[15].style.transform = 'translate(125px, -219px)';
-    i[17].style.transform = 'translate(250px, -146px)';
-    i[19].style.transform = 'translate(250px, 0px)';
-    i[21].style.transform = 'translate(250px, 146px)';
-    i[23].style.transform = 'translate(125px, 219px)';
-    i[25].style.transform = 'translateY(291px)';
-    i[27].style.transform = 'translate(-125px, 219px)';
-    i[29].style.transform = 'translate(-250px, 146px)';
-    i[31].style.transform = 'translate(-250px, 0px)';
-    i[33].style.transform = 'translate(-250px, -146px)';
-    i[35].style.transform = 'translate(-125px, -219px)';
-});
+    i[13].style.transform = 'translateY(-28.6vh)';
+    i[15].style.transform = 'translate(12.6vh, -307%)';
+    i[17].style.transform = 'translate(25.2vh, -206%)';
+    i[19].style.transform = 'translate(25.2vh, 0%)';
+    i[21].style.transform = 'translate(25.2vh, 206%)';
+    i[23].style.transform = 'translate(12.6vh, 307%)';
+    i[25].style.transform = 'translateY(28.6vh)';
+    i[27].style.transform = 'translate(-12.6vh, 307%)';
+    i[29].style.transform = 'translate(-25.2vh, 206%)';
+    i[31].style.transform = 'translate(-25.2vh, 0%)';
+    i[33].style.transform = 'translate(-25.2vh, -206%)';
+    i[35].style.transform = 'translate(-12.6vh, -307%)';
 
-// centerCell.addEventListener('click', togglecells())
-// console.log(i)
+});
